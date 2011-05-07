d634aadf37f7fefbc01091468a448aa3eee53588
Florian Kutschera <kontakt@florian-kutschera.de>
2011-05-07 21:03:26 +0200

    - Struktur aufgeräumt

--- main_views/searchRooms.js
+++ main_views/searchRooms.js
@@ -1,19 +1,71 @@
-var rooms = [];
-var xhr = Titanium.Network.createHTTPClient();
-xhr.onload = function() {
-	rooms_objects = JSON.parse(this.responseText);
-	rooms.push({title: rooms_objects[0].name, hasDetail: true});
-	
-	// create table view
-	var roomsTable = Titanium.UI.createTableView({
-		data:rooms
+var serviceUrl = 'https://mongolab.com/api/1/databases/wickelmich/collections/locations?apiKey=4dbe5e5a50f0458d3ca398cc&limit=20';
+var xhr = Ti.Network.createHTTPClient();
+ 
+xhr.onerror = function(e)
+{
+  Ti.UI.createAlertDialog({title: 'Network Error', message: 'Unable to retrieve data.'}).show();
+  Ti.API.info('NETWORK ERROR ' + e.error);
+};
+ 
+xhr.setTimeout(30000);
+ 
+xhr.openPath = function()
+{
+  this.open('GET', serviceUrl);
+  this.send();
+};
+
+// create table view
+var roomsTable = Titanium.UI.createTableView();
+
+// set event listener for click on table row
+roomsTable.addEventListener('click', function(e) {
+	Ti.UI.createAlertDialog({title: e.name, message: e.message}).show();
+});
+
+// add roomsTable view to the window
+Ti.UI.currentWindow.add(roomsTable);
+
+function createRow(room) {
+	var row = Ti.UI.createTableViewRow({
+		hasDetail: true
 	});
 	
-	// add table view to the window
-	Titanium.UI.currentWindow.add(roomsTable);
+	rowTitle = Ti.UI.createLabel({
+		text: room.title
+	});
+	row.add(rowTitle);
+	
+	rowDistance = Ti.UI.createLabel({
+		text: room.loc
+	});
+	row.add(rowDistance);
+	
+	return row;
+}
+
+xhr.onload = function() {
+	if (this.responseData != '[]') {
+		try {
+			var rooms_objects = JSON.parse(this.responseText);
+		}
+		catch(e) {
+			Ti.UI.createAlertDialog({title: e.name, message: e.message}).show();
+			Ti.API.debug(this.responseData);
+			return;
+		}
+		
+		var rooms = [];
+		
+		for (var i=0; i<=rooms_objects.length; i++) {
+			Ti.API.info(this.rooms_objects[i]);
+			row = createRow({
+				title: rooms_objects[i].title,
+				loc: rooms_objects[i].loc
+			});
+			rooms.push(row);
+		}
+	}
 };
-xhr.onerror = function(e) {
-	Ti.API.info('in utf-8 error for GET with QS:' + e.error);
-};
-xhr.open("GET","https://mongolab.com/api/1/databases/wickelmich/collections/locations?apiKey=4dbe5e5a50f0458d3ca398cc&limit=20");
-xhr.send();
+
+xhr.openPath();
